367ee817f28905f46edcc364ad4ffb00d3c1436e
BRIAN BETANCOURT <bbetancova@gmail.com>
2018-07-23 23:09:12 +0000

    Done.

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -4,7 +4,7 @@ require 'pry'
   # contacts = {
   #   "Jon Snow" => {
   #     name: "Jon",
-  #     email: "jon_snow@thewall.we", 
+  #     email: "jon_snow@thewall.we",
   #     favorite_ice_cream_flavors: ["chocolate", "vanilla"]
   #   },
   #   "Freddy Mercury" => {
@@ -13,7 +13,19 @@ require 'pry'
   #     favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
   #   }
   # }
-  
-def remove_strawberry(contacts)
 
-end
+  def remove_strawberry(contacts)
+  contacts.each do |person, data|
+    #at this level, "person" is Jon Snow or Freddy and "data" is a hash of key/value pairs
+
+    if (person == "Freddy Mercury")
+      i = 0
+      data.each do |attribute, value|
+       if attribute == :favorite_ice_cream_flavors
+       value.shift
+       end #endif conditional
+      end #end data.each iteration
+    end #end person conditional
+  end #end contacts.each iteration
+
+  end #end definition
